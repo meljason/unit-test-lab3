c69c80838d112c0ede182cc7ea44f26a019f127f
ese-omene <55164935+ese-omene@users.noreply.github.com>
2020-01-23 11:01:31 -0500

    Update Lab-3-unittest-function.js

--- Lab-3-unittest-function.js
+++ Lab-3-unittest-function.js
@@ -1,8 +1,19 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
-alert("lab 3 Function");//please delete this line once connected.
+//alert("lab 3 Function");//please delete this line once connected.
 /** checkHumbrId Function
  * Validate input as a Humber College Student number.
  * Returns true if input validates.
  * @param {string} idIn
  */
+ function checkHumbrid(HumbrIn){
+   "use strict";
+   let validID = false;
+   if (isNan(HumbrIn) || HumberIn === undefined){
+     validID = false;
+   }else if (HumberIn.match(/(n|N)\d{8}/)){
+     validID = true;
+   }
+return validID;
+
+ }
