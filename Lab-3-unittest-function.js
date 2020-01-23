17d35d30043ca1c23c081999641257313901a147
Jason Chong <jasoncwy@hotmail.com>
2020-01-23 11:58:32 -0500

    add unit test

--- Lab-3-unittest-function.js
+++ Lab-3-unittest-function.js
@@ -1,19 +1,26 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
-//alert("lab 3 Function");//please delete this line once connected.
+// alert("lab 3 Function");//please delete this line once connected.
 /** checkHumbrId Function
  * Validate input as a Humber College Student number.
  * Returns true if input validates.
  * @param {string} idIn
  */
- function checkHumbrid(HumbrIn){
-   "use strict";
-   let validID = false;
-   if (isNan(HumbrIn) || HumberIn === undefined){
-     validID = false;
-   }else if (HumberIn.match(/(n|N)\d{8}/)){
-     validID = true;
-   }
-return validID;
+function checkHumbrid(idIn) {
+  "use strict";
 
- }
+ 
+  // regex = new RegExp(idIn, '/(n|N)\d{8}/');
+
+  let validID = false;
+
+  if (!isNaN(idIn) || idIn === undefined) {
+    validID = false;
+  } 
+
+  else if (/^(n|N)\d{8}$/.test(idIn)) {
+    validID = true;
+  }
+  return validID;
+
+}
--- Lab-3-unittest-test.js
+++ Lab-3-unittest-test.js
@@ -1,4 +1,41 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE UNIT TEST ==========
-alert("lab 3 UNIT TEST");//please delete this line once connected.
+//  alert("lab 3 UNIT TEST");//please delete this line once connected.
 
+function test__checkHumbrID(valueIn, expected) {
+    "use strict";
+
+    var msgPass = " ==PASSED==";
+    var msgFail = " xxFAILEDxx";
+    var msg;
+    
+    let result = checkHumbrid(valueIn);
+
+    // let msg = "Value tested: " + valueIn + 
+    //           " | Tested Result: " + result + 
+    //           " | Expected Result: " + expected + "<br/>";
+
+    if (result == expected) { 
+        msg = "Value tested: " + valueIn +
+            " | Expected Result: " + expected + "<span style='color: green;'>" + msgPass + "</span><br/>";
+    } 
+
+    else { 
+        msg = "Value tested: " + valueIn +
+            " | Expected Result: " + expected + "<span style='color: red;'>" + msgFail + "</span><br/>";
+    }
+
+    let msgBox = document.getElementById("data");
+    msgBox.innerHTML += msg;
+
+    // console.log(msgBox)
+
+}
+
+test__checkHumbrID(undefined, false); //test to fail with known failing value
+test__checkHumbrID("n01234567", true); //test to pass
+test__checkHumbrID("N01234567", true); //test to pass
+test__checkHumbrID("x", false); //test fail
+test__checkHumbrID("n012345678", false); //test to fail
+test__checkHumbrID("n0123456", false); //test to fail
+test__checkHumbrID("n01355438", false); //test to fail
